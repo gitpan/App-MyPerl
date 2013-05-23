982c033dae32c0119ad31e73f7d0af39e9ade31a
Matt S Trout <perl-stuff@trout.me.uk>
2013-05-23 19:21:56 +0000

    Import of MSTROUT/App-MyPerl-0.002000 from CPAN.
    gitpan-cpan-distribution: App-MyPerl
    gitpan-cpan-version:      0.002000
    gitpan-cpan-path:         MSTROUT/App-MyPerl-0.002000.tar.gz
    gitpan-cpan-author:       MSTROUT
    gitpan-cpan-maturity:     released

--- lib/App/MyPerl/Rewrite.pm
+++ lib/App/MyPerl/Rewrite.pm
@@ -3,9 +3,11 @@ package App::MyPerl::Rewrite;
 use Moo;
 use IO::All;
 
-extends 'App::MyPerl';
+with 'App::MyPerl::Role::Script';
 
-has file_preamble => (is => 'lazy', builder => sub {
+sub use_files { qw(modules) }
+
+has exclude_dev_preamble => (is => 'lazy', builder => sub {
   join "\n", @{$_[0]->preamble},
 });
 
@@ -16,7 +18,8 @@ sub run {
 
 sub rewrite_dir {
   my ($self, $dir) = @_;
-  my $preamble = $self->file_preamble;
+  my $preamble = $self->exclude_dev_preamble;
+  print $preamble . "\n" if $self->_env_value('DEBUG');
   foreach my $file (io->dir($dir)->all_files(0)) {
     next unless $file->name =~ /\.pm$|\.t$|^${dir}\/bin\//;
     my $data = $file->all;
