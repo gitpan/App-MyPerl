f694d2fdbe132f090eb1051c6d6d86cf80b0805c
Matt S Trout <perl-stuff@trout.me.uk>
2013-07-20 18:02:49 +0000

    Import of MSTROUT/App-MyPerl-0.003000 from CPAN.
    gitpan-cpan-distribution: App-MyPerl
    gitpan-cpan-version:      0.003000
    gitpan-cpan-path:         MSTROUT/App-MyPerl-0.003000.tar.gz
    gitpan-cpan-author:       MSTROUT
    gitpan-cpan-maturity:     released

--- lib/App/MyPerl/Role/Script.pm
+++ lib/App/MyPerl/Role/Script.pm
@@ -68,39 +68,53 @@ has config_dirs => (is => 'lazy', builder => sub {
 sub use_files { qw(dev-modules modules) }
 
 sub _files_for {
-  my ($self, $dir) = @_;
-  map $dir->catfile($_), $self->use_files
+  my ($self, $dir, $prefix) = @_;
+  map $dir->catfile($prefix.$_), $self->use_files
 }
 
-has modules => (is => 'lazy', builder => sub {
+sub _build_module_list {
+  my ($self, $prefix) = @_;
   [ grep !/^#/ && !/^\s*$/,
       map $_->chomp->slurp,
         grep $_->exists,
-          map $_[0]->_files_for($_),
-              @{$_[0]->config_dirs}
+          map $self->_files_for($_, $prefix),
+              @{$self->config_dirs}
   ]
+}
+
+has modules => (is => 'lazy', builder => sub {
+  $_[0]->_build_module_list('')
+});
+
+has script_modules => (is => 'lazy', builder => sub {
+  $_[0]->_build_module_list('script-')
 });
 
 has preamble => (is => 'lazy', builder => sub {
+  $_[0]->_preamble_from_modules(@{$_[0]->modules})
+});
+
+sub _preamble_from_modules {
+  my ($self, @modules) = @_;
   [ map {
           my ($mod, $arg) = split('=', $_, 2);
-          my $usenouse = "use";
+          my $use_or_no = "use";
           if ($mod =~ /^-/) {
-            $usenouse = "no";
+            $use_or_no = "no";
             $mod =~ s/^-//;
           }
           ($arg
-                ? "$usenouse ${mod} qw(".join(' ', split ',', $arg).");"
-                : "$usenouse ${mod};")
-    } @{$_[0]->modules}
+                ? "$use_or_no ${mod} qw(".join(' ', split ',', $arg).");"
+                : "$use_or_no ${mod};")
+    } @modules
   ]
-});
+}
 
 has perl_options => (is => 'lazy', builder => sub {
   my ($self) = @_;
   [
     "-Mlib::with::preamble=${\join(' ', @{$self->preamble})},lib,t/lib",
-    (map "-M$_", @{$self->modules})
+    (map "-M$_", @{$self->script_modules}, @{$self->modules})
   ];
 });
 
